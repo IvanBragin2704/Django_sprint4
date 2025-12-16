2c6f25adb8f52d5dcf594864a9604dbf32801509
cherry-pushka <i.am.bragin@gmail.com>
2025-12-16 13:18:21 +0400

    Первоначальный коммит с незначительными ошибками

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,13 +1,29 @@
 from django import forms
-
-from .models import Post, Comment, User
+from .models import Post, Comment, User, Location, Category
 
 
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Post
         exclude = ('author',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['location'].required = False
+        self.fields['category'].required = False
+
+        self.fields['location'].queryset = (
+            Location.objects.all().order_by('name'))
+        self.fields['category'].queryset = (
+            Category.objects.all().order_by('title'))
+
+        self.fields['location'].empty_label = "Выберите местоположение"
+        self.fields['category'].empty_label = "Выберите категорию"
+
+        self.fields['pub_date'].widget = forms.DateTimeInput(
+            attrs={'type': 'datetime-local', 'class': 'form-control'}
+        )
 
 
 class CommentForm(forms.ModelForm):
